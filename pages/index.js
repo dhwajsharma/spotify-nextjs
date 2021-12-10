3455fee151113f8a3e9506527a9877db98617945
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-12-11 00:14:46 +0530

    Implementing nextAuth authentication

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
-import Head from "next/head";
+import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Spotify</title>
-      </Head>
-
-      <main></main>
+    <div className="bg-black h-screen overflow-hidden">
+      <main className="">
+        <Sidebar />
+      </main>
     </div>
   );
 }
